29b84a92791cd694e94d76035ef0c7d721b24776
박태양 <9001025@partner.onestorecorp.com>
2022-03-10 13:26:40 +0900

    11654

---
+++ src/BAEKJOON_11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BAEKJOON_11654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char chr = sc.next().charAt(0);
+
+        System.out.println((int)chr);
+
+    }
+}
